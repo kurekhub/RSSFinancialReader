4922f2619d2dff4c2a1452690b5503ca4fc7a2b2
Michał Ligęza <mligeziak@gmail.com>
2017-05-27 22:04:14 +0200

    add info

--- app/src/main/java/com/kurekhub/rssfinancialreader/InfoFragment.java
+++ app/src/main/java/com/kurekhub/rssfinancialreader/InfoFragment.java
@@ -6,8 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SettingsFragment extends Fragment {
+public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 }
